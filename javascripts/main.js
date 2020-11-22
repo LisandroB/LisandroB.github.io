d4068334e1944b4c0a8ef83ed1a93285a93f9c2f
LBrasolin <brasolinlisandro0@gmail.com>
2020-11-22 00:01:19 -0300

    cheto

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,18 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.color = "rgb(153, 153, 153)";
+    document.getElementById("pesado").style.opacity = "1";
     document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.transition = "0.5s"
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("mySidenav").style.transition = "0.5s";
+    document.getElementById("pesado").style.opacity = "0";
+    document.getElementById("pesado").style.transition = "all 0.9s ease .45s;";
+    document.body.style.transition = "0.5s"
     document.body.style.backgroundColor = "white";
   }
